c9c1ad6733c3ba1c26cbc00daaf89d724e525335
Mahmod94 <78415546+Mahmod94@users.noreply.github.com>
2026-04-09 10:51:54 +0200

    Update Layout.tsx
    fixed navbar

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,25 +2,36 @@ import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
 import { Link as RouterLink, Outlet } from "react-router-dom";
 
 export default function Layout() {
-    return (
-        <>
-            <AppBar position="static" elevation={0} sx={{ background: "#fff", color: "#000"}}>
-                <Toolbar sx={{ justifyContent: "space-between"}}>
-                    <Typography variant="h6" fontWeight={700}>
-                        Mahmod
-                    </Typography>
-                    
-                    <Box>
-                        <Button component={RouterLink} to="/">Home</Button>
-                        <Button component={RouterLink} to="/projects">Projects</Button>
-                        <Button component={RouterLink} to="/contact">Contact</Button>
-                    </Box>
-                </Toolbar>
-            </AppBar>
+  return (
+    <>
+      <AppBar
+        position="fixed"
+        elevation={0}
+        sx={{
+          backgroundColor: "#fff",
+          color: "#000",
+          borderBottom: "1px solid",
+          borderColor: "divider",
+        }}
+      >
+        <Toolbar sx={{ justifyContent: "space-between" }}>
+          <Typography variant="h6" fontWeight={700}>
+            Mahmod
+          </Typography>
 
-            <Box>
-                <Outlet />
-            </Box>
-        </>
-    );
+          <Box>
+            <Button component={RouterLink} to="/">Home</Button>
+            <Button component={RouterLink} to="/projects">Projects</Button>
+            <Button component={RouterLink} to="/contact">Contact</Button>
+          </Box>
+        </Toolbar>
+      </AppBar>
+
+      <Toolbar />
+
+      <Box>
+        <Outlet />
+      </Box>
+    </>
+  );
 }
